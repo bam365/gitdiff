789913d7b546d04af4933483de8f5163e34ac2b8
bam365 <bam365@mst.edu>
2012-01-31 19:02:51 +0000

    Added signal handling, function reduction

--- gitdiff.c
+++ gitdiff.c
@@ -2,28 +2,70 @@
  * Blake Mitchell, 2012
  */
 
-#include <stdio.h>
+#include <stdlib.h>
 #include <curses.h>
+#include <signal.h>
 
 #define ARRYSIZE(x) (sizeof(x)/sizeof(x[0]))
 
+
+void init_curses();
 void print_strings(char *strings[], int size);
+void set_signal_handlers();
 void ev_loop(int size);
+void end_curses();
+void handle_resize(int sig);
+void handle_sigint(int sig);
+
+
+static int CURSES_SCREEN = 0;
+
 
 main()
 {
         char *strings[] = {"Hello", "There", "World"};
 
-        initscr();
-        cbreak();
-        noecho();
+        set_signal_handlers();
+        init_curses();
         print_strings(strings, ARRYSIZE(strings));
-        refresh();
-        
         ev_loop(ARRYSIZE(strings));
-
-        endwin();
+        end_curses();
 }
+
+
+void set_signal_handlers()
+{
+        struct sigaction sa_winch;
+        struct sigaction sa_int;
+
+        /*TODO: Error checking, bitch */
+        sigfillset(&sa_winch.sa_mask);
+        sigfillset(&sa_int.sa_mask);
+        sa_winch.sa_flags = sa_int.sa_flags = 0;
+        sigaction(SIGINT, &sa_int, 0);
+        sigaction(SIGWINCH, &sa_winch, 0);
+}
+
+
+void init_curses()
+{
+        if (!CURSES_SCREEN) {
+                initscr();
+                cbreak();
+                noecho();
+                CURSES_SCREEN = 1;
+        }
+}
+
+
+void end_curses()
+{
+        if (CURSES_SCREEN) {
+                endwin();
+                CURSES_SCREEN = 0;
+        }
+}
+
 
 void print_strings(char *strings[], int size) 
 {
@@ -33,6 +75,7 @@ void print_strings(char *strings[], int size)
                 mvprintw(i, 0, "%s\n", strings[i]);
         }
 }
+
 
 void ev_loop(int size)
 {
@@ -63,4 +106,17 @@ void ev_loop(int size)
                         ref = 0;
                 }
         }
+}
+
+
+void handle_resize(int sig)
+{
+        /*TODO: Stuff. */
+}
+
+
+void handle_int(int sig)
+{
+        end_curses();
+        exit(0);
 }
